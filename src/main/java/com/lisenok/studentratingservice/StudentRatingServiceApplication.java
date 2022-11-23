4fbe9d3be32fd2ddfb9d9461599d6529df08c633
lisseenok <45551611+lisseenok@users.noreply.github.com>
2022-11-23 19:38:34 +0300

    Initial commit

---
+++ src/main/java/com/lisenok/studentratingservice/StudentRatingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lisenok.studentratingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRatingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentRatingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lisenok/studentratingservice/StudentRatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lisenok.studentratingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRatingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
