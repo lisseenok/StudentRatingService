e646fbe6b3daf47496fafe53619bb64e138d6dbb
lisseenok <45551611+lisseenok@users.noreply.github.com>
2022-11-28 17:36:34 +0300

    FEATURE: получение рейтингов студента

--- src/main/java/com/lisenok/studentratingservice/service/StudentService.java
+++ src/main/java/com/lisenok/studentratingservice/service/StudentService.java
@@ -1,14 +1,18 @@
 package com.lisenok.studentratingservice.service;
 
+import com.lisenok.studentratingservice.domain.dto.RatingResponseDTO;
 import com.lisenok.studentratingservice.domain.dto.StudentDTO;
 import com.lisenok.studentratingservice.domain.model.Student;
+import com.lisenok.studentratingservice.mapper.RatingMapper;
 import com.lisenok.studentratingservice.mapper.StudentMapper;
 import com.lisenok.studentratingservice.problem.StudentNotFoundProblem;
 import com.lisenok.studentratingservice.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +21,8 @@ public class StudentService {
     private final StudentRepository studentRepository;
 
     private final StudentMapper studentMapper;
+
+    private final RatingMapper ratingMapper;
 
     public Optional<Student> findById(int id) {
         return studentRepository.findById(id);
@@ -28,6 +34,11 @@ public class StudentService {
                 .orElseThrow(() -> new StudentNotFoundProblem(id));
     }
 
+    public Student getEntityById(int id) {
+        return findById(id)
+                .orElseThrow(() -> new StudentNotFoundProblem(id));
+    }
+
     public StudentDTO save(StudentDTO studentDTO) {
         return studentMapper.toDto(studentRepository.save(studentMapper.toEntity(studentDTO)));
     }
@@ -36,6 +47,11 @@ public class StudentService {
         findById(id).map(studentMapper::toDto)
                 .orElseThrow(() -> new StudentNotFoundProblem(id));
         return save(studentDTO);
+    }
+
+    public List<RatingResponseDTO> getRatings(int id) {
+        Student student = getEntityById(id);
+        return student.getRatings().stream().map(ratingMapper::toDto).collect(Collectors.toList());
     }
 
 }
