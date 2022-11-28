02e825547238a4a3908c8e398aeb4a719f155967
lisseenok <45551611+lisseenok@users.noreply.github.com>
2022-11-28 17:33:17 +0300

    FEATURE: метод добавления студента на курс

--- src/main/java/com/lisenok/studentratingservice/service/CourseService.java
+++ src/main/java/com/lisenok/studentratingservice/service/CourseService.java
@@ -2,6 +2,7 @@ package com.lisenok.studentratingservice.service;
 
 import com.lisenok.studentratingservice.domain.dto.CourseDTO;
 import com.lisenok.studentratingservice.domain.model.Course;
+import com.lisenok.studentratingservice.domain.model.Student;
 import com.lisenok.studentratingservice.mapper.CourseMapper;
 import com.lisenok.studentratingservice.problem.CourseNotFoundProblem;
 import com.lisenok.studentratingservice.repository.CourseRepository;
@@ -17,6 +18,8 @@ public class CourseService {
 
     private final CourseMapper courseMapper;
 
+    private final StudentService studentService;
+
     public Optional<Course> findById(int id) {
         return courseRepository.findById(id);
     }
@@ -27,13 +30,29 @@ public class CourseService {
                 .orElseThrow(() -> new CourseNotFoundProblem(id));
     }
 
+    public Course getEntityById(int id) {
+        return findById(id)
+                .orElseThrow(() -> new CourseNotFoundProblem(id));
+    }
+
+    public CourseDTO save(Course course) {
+        return courseMapper.toDto(courseRepository.save(course));
+    }
+
     public CourseDTO save(CourseDTO courseDTO) {
-        return courseMapper.toDto(courseRepository.save(courseMapper.toEntity(courseDTO)));
+        return save(courseMapper.toEntity(courseDTO));
     }
 
     public CourseDTO update(CourseDTO courseDTO, int id) {
         findById(id).map(courseMapper::toDto)
                 .orElseThrow(() -> new CourseNotFoundProblem(id));
         return save(courseDTO);
+    }
+
+    public CourseDTO addStudent(int courseId, int studentId) {
+        Course course = getEntityById(courseId);
+        Student student = studentService.getEntityById(studentId);
+        course.addStudent(student);
+        return save(course);
     }
 }
